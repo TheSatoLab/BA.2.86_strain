5aff83890861bd96851f8878585b3a9de5f701dc
The Sato Lab <54346922+TheSatoLab@users.noreply.github.com>
2023-11-01 17:24:01 +0900

    Delete script directory

--- script/metadata/BA.2.86_nextclade_metadata_prep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#$ -S /bin/bash
-#$ -pe def_slot 18
-#$ -l exclusive,s_vmem=10G,mem_req=10G
-##$ -o /dev/null
-##$ -e /dev/null
-##$ -t 1-2:1
-
-cut -f 1-8 nextclade.tsv > nextclade.cut.tsv
